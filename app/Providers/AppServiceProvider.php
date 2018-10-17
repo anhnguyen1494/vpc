6225952201d82dc817800b5725471d348eec9d9f
TuanAnh <anhnguyen1494@gmail.com>
2018-10-17 16:15:12 +0700

    update order danh muc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $categorys = Category::with('products')->get();
+        $categorys = Category::with('products')->orderBy('order')->get();
         $sets = Setting::all();
         $settings = [];
         foreach ($sets as $setting){
